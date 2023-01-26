482edd5e15d465ff79b8193474d45acf62ad607c
Wojciech Podmokły <wpodmokly@gmail.com>
2023-01-26 23:36:00 +0100

    init

---
+++ client/src/OtherPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      multi-fib
+      <br />
+      Wojciech Podmokly
+      <br />
+      I2S 2.3
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
